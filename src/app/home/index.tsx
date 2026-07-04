e6fcb4a2250beb3b14b082a70665f1a7c45c93ad
Julius Christoffersen <juliuschris@me.com>
2026-07-04 19:53:45 +0800

    feat: get and display customer relationships on the home screen

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,9 +1,30 @@
+import { useCustomerRelationships } from '@/features/customer-relationships/hooks/queries/useCustomerRelationships';
 import { Text, View } from 'react-native';
 
 export default function Home() {
+  const { data, isLoading, error } = useCustomerRelationships();
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>
+          Error: {error instanceof Error ? error.message : 'Unknown error'}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>Blah</Text>
+      <Text>Points: {data?.points}</Text>
     </View>
   );
 }
