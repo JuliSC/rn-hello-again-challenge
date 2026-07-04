c0cc3e1687dbfa3aee85b5d898a0d0561a2d9640
Julius Christoffersen <juliuschris@me.com>
2026-07-04 19:38:27 +0800

    feat: set token storage on successful login

--- src/features/auth/hooks/mutations/useLogin.ts
+++ src/features/auth/hooks/mutations/useLogin.ts
@@ -1,9 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
 import { login } from '../../api/login';
+import { tokenStorage } from '../../storage/token-storage';
 import { LoginRequest } from '../../types';
 
 export const useLogin = () =>
   useMutation({
     mutationFn: ({ email, password }: LoginRequest) =>
       login({ email, password }),
+    onSuccess: async (response) => {
+      await tokenStorage.setToken(response.token);
+    },
   });
