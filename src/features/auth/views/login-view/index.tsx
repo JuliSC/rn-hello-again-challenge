33c6501380b0f5bfaf47c2018c9f5e5c1bd18d40
Julius Christoffersen <juliuschris@me.com>
2026-07-04 19:39:13 +0800

    feat: login and navigate with hardcoded data

--- src/features/auth/views/login-view/index.tsx
+++ src/features/auth/views/login-view/index.tsx
@@ -1,17 +1,29 @@
 import { BorderRadius, Spacing } from '@/constants/theme';
 import { useTheme } from '@/hooks/use-theme';
 import { useRouter } from 'expo-router';
+import { useCallback } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useLogin } from '../../hooks/mutations/useLogin';
 
 export default function LoginView() {
   const router = useRouter();
   const theme = useTheme();
+  const { mutateAsync: login } = useLogin();
+
+  const handleLogin = useCallback(async () => {
+    try {
+      await login({ email: 'testUser@dev.null', password: 'challenge-2026;' });
+      router.replace('/home');
+    } catch (err) {
+      console.log('Login error:', err);
+    }
+  }, [login, router]);
 
   return (
     <View style={styles.container}>
       <Pressable
         style={[styles.button, { backgroundColor: theme.primary }]}
-        onPress={() => router.replace('/home')}
+        onPress={handleLogin}
       >
         <Text style={styles.buttonText}>Login</Text>
       </Pressable>
