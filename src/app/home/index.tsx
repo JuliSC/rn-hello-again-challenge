aa09df4ce1818467217973dd329935393ab224b6
Julius Christoffersen <juliuschris@me.com>
2026-07-05 09:36:49 +0800

    feat: get and display bounties on the home screen

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,3 +1,4 @@
+import { useBounties } from '@/features/bounties/hooks/queries/useBounties';
 import { useCustomerRelationships } from '@/features/customer-relationships/hooks/queries/useCustomerRelationships';
 import { useProfile } from '@/features/profile/hooks/queries/useProfile';
 import { Text, View } from 'react-native';
@@ -15,7 +16,13 @@ export default function Home() {
     error: profileError,
   } = useProfile();
 
-  if (customerRelationshipsLoading || profileLoading) {
+  const {
+    data: bountiesData,
+    isLoading: bountiesLoading,
+    error: bountiesError,
+  } = useBounties();
+
+  if (customerRelationshipsLoading || profileLoading || bountiesLoading) {
     return (
       <View>
         <Text>Loading...</Text>
@@ -23,7 +30,7 @@ export default function Home() {
     );
   }
 
-  if (customerRelationshipsError || profileError) {
+  if (customerRelationshipsError || profileError || bountiesError) {
     return (
       <View>
         <Text>
@@ -33,6 +40,9 @@ export default function Home() {
             : 'Unknown error'}
           {profileError instanceof Error
             ? profileError.message
+            : 'Unknown error'}
+          {bountiesError instanceof Error
+            ? bountiesError.message
             : 'Unknown error'}
         </Text>
       </View>
@@ -48,6 +58,17 @@ export default function Home() {
       <Text>Last Name: {profileData?.last_name}</Text>
       <Text>Locale: {profileData?.locale}</Text>
       <Text>Customer ID: {profileData?.customer_id}</Text>
+      <Text>Bounties:</Text>
+      {bountiesData?.map((bounty) => (
+        <View key={bounty.id}>
+          <Text>ID: {bounty.id}</Text>
+          <Text>Name: {bounty.name}</Text>
+          <Text>Description: {bounty.description}</Text>
+          <Text>Is Redeemable: {bounty.is_redeemable ? 'Yes' : 'No'}</Text>
+          <Text>Needed Points: {bounty.needed_points}</Text>
+          <Text>CR Points: {bounty.cr_points}</Text>
+        </View>
+      ))}
     </View>
   );
 }
